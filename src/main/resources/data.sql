a207a42069e02f0b9929b1ba581844f03d16f110
shashimal <shasimald@gmail.com>
2018-10-15 20:15:29 +0800

    Updated data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,11 @@
-insert into product values(1,'Apple Watch', 10,20000);
-insert into product values(2,'iPhoneX', 5,7500);
+insert into product (ID,NAME,QUANTITY,SALE_AMOUNT) values(1,'Samsung Galaxy Tab A', 10,5990.00);
+insert into product (ID,NAME,QUANTITY,SALE_AMOUNT) values(2,'iPhone 7+', 15,15000.00);
+insert into product (ID,NAME,QUANTITY,SALE_AMOUNT) values(3,'iPhone 10X', 3,4500.00);
+insert into product (ID,NAME,QUANTITY,SALE_AMOUNT) values(4,'Apple Watch', 2,1600.00);
+insert into product (ID,NAME,QUANTITY,SALE_AMOUNT) values(5,'Mack Book Pro', 10,50000.00);
+insert into product (ID,NAME,QUANTITY,SALE_AMOUNT) values(6,'E-Scooter', 2,3000.00);
+insert into product (ID,NAME,QUANTITY,SALE_AMOUNT) values(7,'Power Bank', 10,7000.00);
+insert into product (ID,NAME,QUANTITY,SALE_AMOUNT) values(8,'Head Phone', 5,3500.00);
+insert into product (ID,NAME,QUANTITY,SALE_AMOUNT) values(9,'Samsung TV', 3,2100.00);
+insert into product (ID,NAME,QUANTITY,SALE_AMOUNT) values(10,'Samsung LCD Monitor', 4,2400.00);
+
